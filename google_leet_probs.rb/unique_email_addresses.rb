c50f76bf3944dd2e3ab97d21783ed221f6b27bd1
github username <github email address>
2020-10-19 21:38:23 -0600

    adds get_local_name method to unique_email

---
+++ google_leet_probs.rb/unique_email_addresses.rb
@@ -0,0 +1,36 @@
+require 'pry'
+# Input: ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+# Output: 2
+# Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails
+
+# If you add periods ('.') between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name.  For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.  (Note that this rule does not apply for domain names.)
+
+# If you add a plus ('+') in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered, for example m.y+name@email.com will be forwarded to my@email.com.  (Again, this rule does not apply for domain names.)
+
+# It is possible to use both of these rules at the same time.
+
+# Given a list of emails, we send one email to each address in the list.  How many different addresses actually receive mails?
+
+# @param {String[]} emails
+# @return {Integer}
+
+# def num_unique_emails(emails)
+
+# end
+
+#need to slice off local name before @
+# remove all . from local name
+# remove everything after + for local name
+# compare strings
+
+emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+
+def get_local_name(emails)
+    local_names = []
+    emails.each { |email| local_names << email[/[^@]+/] }
+    local_names
+    binding.pry
+end
+# local_names => ["test.email+alex", "test.e.mail+bob.cathy", "testemail+david"]
+
+
