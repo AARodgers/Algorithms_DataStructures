5e3efb672f5fe7fb6b95625b0153c571bbaab0d7
github username <github email address>
2020-10-29 15:14:36 -0600

    adds solution for first_unique_char

---
+++ hashes/first_unique_character_in_a_string.rb
@@ -0,0 +1,36 @@
+require 'pry'
+# Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+
+# Examples:
+
+#Indput:
+s = "leetcode"
+# return 0.
+
+# s = "loveleetcode"
+# return 2.
+
+
+# Note: You may assume the string contains only lowercase English letters.
+
+# @param {String} s
+# @return {Integer}
+
+def first_uniq_char(s)
+    hash = Hash.new
+
+    s.each_char do |i|
+        if hash.has_key?(i)
+            hash[i] += 1
+        else
+            hash[i] = 1
+        end
+    end
+
+    hash.each do |k,v|
+        return s.index(k) if v == 1
+    end
+    return -1
+end
+
+puts first_uniq_char(s)
