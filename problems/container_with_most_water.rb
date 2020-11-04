0bb3e53a02b9a69923a73f4242ecbed6d5c0b7d3
github username <github email address>
2020-11-04 09:06:12 -0700

    adds a solution to container_with_most_water

--- problems/container_with_most_water.rb
+++ problems/container_with_most_water.rb
@@ -1,10 +1,12 @@
+require 'pry'
 # 11. Container With Most Water
 # Medium
 # Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
 
 # Notice that you may not slant the container.
 
-# Input: height = [1,8,6,2,5,4,8,3,7]
+# Input:
+
 # Output: 49
 # Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
 # Example 2:
@@ -23,12 +25,49 @@
 # @param {Integer[]} height
 # @return {Integer}
 
+# def max_area(height)
+#     value_index_hash = {}
+
+#     height.each_with_index do |val, index|
+#         value_index_hash[val] = index
+#     end
+#     # value_index_hash = {1=>0, 8=>6, 6=>2, 2=>3, 5=>4, 4=>5, 3=>7, 7=>8}
+# end
+height = [1,8,6,2,5,4,8,3,7]
+#Sol1 brute force: consider area for every possible pair of the lines and find max. Time: O(N^2), space: O(N)
 def max_area(height)
+    first = 0
+    last = height.size - 1
+
+  max = 0
+
+  while first < last
+    # how far apart the first and last are
+    diff = last - first
+    # the lowest height
+    lowest = [height[first], height[last]].min
+    # the current area
+    current = lowest * diff
 
 
+    max = [max, current].max
+    # whichever side is smaller, we move closer together
+    height[first] == lowest ? first += 1 : last -= 1
+  end
+
+  max
 end
 
+max_area(height)
 
+
+# def highest_two_values(height)
+#     max = height.sort[-1]
+#     min = height.sort[-2]
+#     puts max, min
+# end
+
+# puts highest_two_values(height)
 
 #look for highest value and largest index distance at same time?
 
