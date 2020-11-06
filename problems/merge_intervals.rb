570cf3bd83ac41e83d1b97abd5dda52e9618178a
github username <github email address>
2020-11-06 13:01:59 -0700

    adds final solution to merge_intervals

--- problems/merge_intervals.rb
+++ problems/merge_intervals.rb
@@ -11,16 +11,24 @@
 intervals = [[1,3],[2,6],[8,10],[15,18]]
 
 def merge(intervals)
-    index = 0
-    if intervals[index][-1] > intervals[index + 1][0]
+    return intervals if interval.size == 0
+    intervals = intervals.sort_by { |arr| arr[0]}
+    ans = []
 
-    else
-        false
-    end
+    ans = [intervals[0]]
+        for i in 1..intervals.size - 1
+            if intervals[i][0] <= ans[-1][1]
+                ans[-1][1] = [ans[-1][1], intervals[i][1]].max
+            else
+                ans << intervals[i]
+            end
+        end
+    ans
 end
 
+puts merge(intervals).inspect
 
-puts merge(intervals)
-
-# if the last element in the array is greater than the first element in the next array,
-#merge the arrays and sort them
+#sort intervals array
+# make an array or linked list to add the new sub_arrays to
+# remember to return the array or linked list above
+#iterate through each sub_array
