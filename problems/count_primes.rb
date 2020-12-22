f1579942cc67e444cf404c1435efe882e5d40fc9
Amanda Rodgers <aarodgerswork@gmail.com>
2020-12-21 20:52:37 -0700

    adds hash:

--- problems/count_primes.rb
+++ problems/count_primes.rb
@@ -1,7 +1,7 @@
 # Count the number of prime numbers less than a non-negative number, n.
 
 # Example 1:
-n = 1279
+n = 10
 # Output: 4
 # Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 # Example 2:
@@ -16,24 +16,29 @@ n = 1279
 # @param {Integer} n
 # @return {Integer}
 
-# def count_primes(n)
-
+# def is_prime(n)
+#   sqrt = Math.sqrt(n)
+#   nums = []
+#   nums_below_sqrt = [*1..sqrt].each { |i| nums << i }
+#   nums_below_sqrt.each do |n|
+#     if (sqrt % n) == 0
+#       return false
+#     else
+#       return true
+#     end
+#   end
 # end
 
-# n = 10
-#input  = integer
-#output = boolean
-
-def is_prime (n)
-  sqrt = Math.sqrt(n)
-  nums_below_sqrt = [*1..sqrt].each { |i| p i }
-  nums_below_sqrt.each do |n|
-    if (sqrt % n) == 0
-      return false
-    else
-      return true
-    end
-  end
+def count_primes(n)
+  numbers = []
+  nums_less =  [*0..(n - 1)].each { |i| numbers << i }
+  nums_hash = nums_less.to_h
+  puts nums_hash
 end
 
-puts is_prime(n)
+# 2.6.1 :004 > arr = [[3, 6], [6, 12]]
+#  => [[3, 6], [6, 12]]
+# 2.6.1 :005 > arr.to_h
+#  => {3=>6, 6=>12}
+
+count_primes(n)
