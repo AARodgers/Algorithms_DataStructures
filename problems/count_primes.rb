f75bfbea80da7ef4293aa112f16da721dc414f83
Amanda Rodgers <aarodgerswork@gmail.com>
2020-12-22 20:37:53 -0700

    adds if statement

--- problems/count_primes.rb
+++ problems/count_primes.rb
@@ -1,7 +1,7 @@
 # Count the number of prime numbers less than a non-negative number, n.
 
 # Example 1:
-n = 10
+# n = 10
 # Output: 4
 # Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 # Example 2:
@@ -16,29 +16,56 @@ n = 10
 # @param {Integer} n
 # @return {Integer}
 
-# def is_prime(n)
-#   sqrt = Math.sqrt(n)
-#   nums = []
-#   nums_below_sqrt = [*1..sqrt].each { |i| nums << i }
-#   nums_below_sqrt.each do |n|
-#     if (sqrt % n) == 0
-#       return false
-#     else
-#       return true
-#     end
-#   end
-# end
 
-def count_primes(n)
-  numbers = []
-  nums_less =  [*0..(n - 1)].each { |i| numbers << i }
-  nums_hash = nums_less.to_h
-  puts nums_hash
-end
 
-# 2.6.1 :004 > arr = [[3, 6], [6, 12]]
-#  => [[3, 6], [6, 12]]
-# 2.6.1 :005 > arr.to_h
-#  => {3=>6, 6=>12}
+  n = 10
 
-count_primes(n)
+
+  def count_primes(n)
+    numbers = []
+    nums_less =  [*0..(n - 1)].each { |i| numbers << i }
+    #nums_less = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    prime_nums = []
+    not_prime = []
+
+    if n == 0
+     prime_nums << 0
+    elsif n == 1
+      prime_nums << 0
+    end
+
+    nums_less.map do |n|
+      if n == 2
+        prime_nums << 2
+      elsif n <= 1
+        not_prime << n
+      elsif (n % 2) == 0
+        not_prime << n
+      else
+        prime_nums << n
+      end
+    end
+    prime_nums.count
+  end
+
+  # def is_prime?(n)
+  #   sqrt = Math.sqrt(n)
+  #   nums = []
+  #   nums_below_sqrt = [*1..sqrt].each { |i| nums << i }
+  #   nums_below_sqrt.each do |n|
+  #     if (sqrt % n) == 0
+  #       return false
+  #     else
+  #       return true
+  #     end
+  #   end
+  # end
+
+
+  puts count_primes(n)
+
+
+  # 2.6.1 :004 > arr = [[3, 6], [6, 12]]
+  #  => [[3, 6], [6, 12]]
+  # 2.6.1 :005 > arr.to_h
+  #  => {3=>6, 6=>12}
